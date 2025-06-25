becf1551f5b4bcb705e73ffcc933188522db6730
Dmitry Razvozzhaec <48729817+el-cortador@users.noreply.github.com>
2025-06-25 13:50:01 +0700

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,1 +1,5 @@
-secrets = {'BOT_API_TOKEN': '8137554303:AAG-bdEYsS0jz-8AcWeheCDorAWTNEdY-50'}
+secrets = {
+    'BOT_API_TOKEN': '8137554303:AAG-bdEYsS0jz-8AcWeheCDorAWTNEdY-50',
+    'API_ID': '20357601',
+    'API_HASH': '215e0e78e0a23d7c5e51dcf950a619a3'
+}
